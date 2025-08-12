69c7567ea7e9c28919e3c446444937040a1e1cab
hipex852 <moreira.souza.andre@escola.pr.gov.br>
2025-08-11 22:05:31 -0300

    aa

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Adiciona um "ouvinte de evento" ao botão com o ID 'cta-button'
+    const ctaButton = document.getElementById('cta-button');
 
+    ctaButton.addEventListener('click', () => {
+        // Quando o botão for clicado, esta função será executada
+        alert('Você clicou no botão! Ação do JavaScript.');
+    });
+});
